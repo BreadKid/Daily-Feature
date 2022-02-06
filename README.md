8cceb2ed59665a7cc8e734dae7ba659e03663b92
BreadKid <85332693@qq.com>
2022-02-06 20:31:46 +0800

    [init]:gradle

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Daily-Feature
+
 开发常用功能实现
+
+包管理：```Gradle```
---
+++ src/test/java/com/breadykid/dailyfeature/DailyFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadykid.dailyfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
